6cb002a50a65c70900a3896d25d077e40ab4d46f
Keinwind <KeinwindEichenSchwert@proton.me>
2025-09-15 10:44:51 +0200

    asdfghjkl

--- 1-3/script.js
+++ 1-3/script.js
@@ -1,3 +1,5 @@
+var money = 10;
+
 function randomNumber(min, max) {
   return Math.floor(Math.random() * max + min);
 }
@@ -17,16 +19,47 @@ function rollDices() {
   });
 
   if (hasWon) {
-    console.log("You won!");
+    money ++;
+    alert("You won and have " + money + "€");
     document.body.style.backgroundColor = "green";
     document.body.style.color = "white";
+    
   } else {
-    console.log("You lost!");
+    money --;
+    alert("You lost and have " + money + "€");
     document.body.style.backgroundColor = "red";
     document.body.style.color = "white";
   }
 
+  if (money <= 0) {
+    alert("You are out of money! Game over. \n never gonna give you up!");
+    rolldicebutton.disabled = true;
+  }
+
   console.log(dices);
 }
+
+function placeBet() {
+  let betinput = document.getElementById("betinput");
+  let bet = parseInt(betinput.value);
+
+  if (isNaN(bet) || bet <= 0) {
+    alert("Please enter a valid bet amount.");
+    return;
+  }
+
+  if (bet > money) {
+    alert("You cannot bet more than you have!");
+    return;
+  }
+
+  money -= bet;
+  alert("You placed a bet of " + bet + "€. You now have " + money + "€ left.");
+}
+
+let rolldicebutton = document.getElementById("rolldicebutton");
+let betinput = document.getElementById("betinput");
+
+betinput.addEventListener("change", placeBet);
 
 rolldicebutton.addEventListener("click", rollDices);
