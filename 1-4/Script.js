c646be1d72aea3c5e2640ed3d7697b22fc1a0244
Keinwind <KeinwindEichenSchwert@proton.me>
2025-09-16 13:44:56 +0200

    Implement upgrade system and enhance cookie clicker functionality

--- 1-4/Script.js
+++ 1-4/Script.js
@@ -8,6 +8,8 @@ function updateHtml() {
 
 function save(){
     localStorage.setItem("cookies", currentCookieCount);
+    let upgradeData = JSON.stringify(upgradeTypes);
+    localStorage.setItem("upgradeData", upgradeData);
 }
 
 function CookieClicked() {
@@ -22,21 +24,37 @@ resetButton.addEventListener("click", () => {
     currentCookieCount = 1;
     save();
     updateHtml();
+    upgradeTypes.forEach(upgrade => upgrade.upgradeLevel = 0);
 })
 
 let upgradeTypes = [
-    { name: "👩‍🦳", updateFreq: 5, amount: 1 },
-    { name: "👨‍🌾", updateFreq: 10, amount: 5 },
+    { name: "👩‍🦳", updateFreq: 1, amount: 0.1, upgradeLevel: 0, },
+    { name: "👨‍🌾", updateFreq: 5, amount: 5, upgradeLevel: 0 },
 ]
+let storedData = localStorage.getItem("upgradeData");
+if (storedData != null) {
+    upgradeTypes = JSON.parse(storedData);
+}
 
 for (let i = 0; i < upgradeTypes.length; i++) {
     const upgradeType = upgradeTypes[i];
     let upgradeContainer = document.createElement("div");
     upgradeContainer.innerHTML = upgradeType.name;
-
     let upgradeButton = document.createElement("button");
+
+    upgradeButton.addEventListener("click", () => {
+        upgradeType.upgradeLevel += 1;
+        upgradeButton.innerText = "Buy Upgrade (" + upgradeType.upgradeLevel + ")";
+    });
+
     upgradeButton.innerText= "Buy Upgrade";
     upgradeContainer.appendChild(upgradeButton);
 
     upgrades.appendChild(upgradeContainer);
+
+    setInterval(() => {
+        currentCookieCount += upgradeType.upgradeLevel * upgradeType.amount;
+        save();
+        updateHtml();
+    }, 1000 * upgradeType.updateFreq);
 }
