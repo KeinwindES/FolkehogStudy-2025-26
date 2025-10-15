b59ef9cb11415215bbd929407b445aaed94822d3
Keinwind <KeinwindEichenSchwert@proton.me>
2025-10-15 10:21:03 +0200

    Füge Quiz-Daten hinzu und aktualisiere die HTML-Struktur: Entferne leere Dateien und passe die Verlinkungen an.

---
+++ 2-2/script.js
@@ -0,0 +1,20 @@
+const quizData = [
+    {
+        question: "What name starts with b?",
+        options: [
+            "bob",
+            "paul",
+            "alex",
+        ],
+        correctAnwer: "bob"
+    },
+    {
+        question: "What is this?",
+        options: [
+            "a quiz",
+            "patric",
+            "oil",
+        ],
+        correctAnwer: "a quiz"
+    }
+]
