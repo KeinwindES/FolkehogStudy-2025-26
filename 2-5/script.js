0fb6ad6a271e690bee645657fdadf716a903031d
Keinwind <KeinwindEichenSchwert@proton.me>
2025-10-22 09:56:19 +0200

    feat: füge Setup-Funktion und Zeichenlogik in script.js hinzu; füge .gitignore und VCS-Konfigurationen hinzu

--- 2-5/script.js
+++ 2-5/script.js
@@ -3,6 +3,23 @@ const ctx = canvas.getContext('2d');
 
 
 // SETUP & RESIZE LOGIC
+function Setup(){
+    const DPI = window.devicePixelRatio || 1;
+    canvas.width = window.innerWidth * DPI;
+    canvas.height = window.innerHeight * DPI;
+    
+    ctx.setTransform(DPI, 0,0,DPI,0,0);
+    ctx.lineWidth = 4;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = '#000'
 
-
+}
+Setup();
+window.addEventListener('resize', Setup);
+ctx.beginPath();
+ctx.moveTo(5, 20);
+ctx.lineTo(5, 50);
+ctx.stroke();
+ctx.closePath();
 // DRAWING LOGIC
