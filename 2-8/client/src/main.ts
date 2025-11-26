4fe111558b2964b98a13070215f55b7ed96884f6
Keinwind <KeinwindEichenSchwert@proton.me>
2025-11-26 13:19:20 +0100

    dwadwa

--- 2-8/client/src/main.ts
+++ 2-8/client/src/main.ts
@@ -2,14 +2,23 @@ import "./style.css";
 
 const websocket = new WebSocket("ws://127.0.0.1:8080");
 
+const output = document.getElementById("output") as HTMLDivElement;
+const textInput = document.getElementById("textfield") as HTMLInputElement;
+const submitButton = document.getElementById("submitbutton") as HTMLButtonElement;
+
+submitButton.addEventListener("click", () => {
+  websocket.send(textInput.value);
+  textInput.value = "";
+});
+
 websocket.addEventListener("open", (data) => {
   console.log("WebSocket opened", data);
-  setInterval(() => {
-    websocket.send("Hello from client!");
-  }, 1000);
+  websocket.send("Hello from client!");
   websocket.send("ekploder");
 });
-websocket.addEventListener("message", (data) => {
+websocket.addEventListener("message", (event) => {
+  const data = event.data;
+  output.innerText = data
   console.log("Received message:", data);
 });
 websocket.addEventListener("close", (data) => {
--- 2-8/server/index.ts
+++ 2-8/server/index.ts
@@ -7,6 +7,8 @@ const wss = new WebSocketServer({
 const connections = new Map<number, WebSocket>();
 let connectionIdCounter = 0;
 
+let messages = "";
+
 wss.on("connection", (ws) => {
   console.log("connection");
 
@@ -14,22 +16,16 @@ wss.on("connection", (ws) => {
   connectionIdCounter++;
   connections.set(id, ws);
 
-  ws.on("message", (data) => {
-    const message = data.toString();
-    switch (message) {
-      case "hei server!!":
-        console.log("hi");
-        break;
-      case "ekploder":
-        ws.send("boom");
-        connections.forEach((connection, connectionID) => {
-          connection.send("");
-        });
-        console.log("boom did run");
-        break;
-    }
-    console.log("Message", data.toString());
-  });
+  ws.on("message", data => {
+          messages += "\n"
+          messages += id + " - " + data.toString();
+          connections.forEach((sock,key) => {
+              sock.send(messages);
+          })
+
+          console.log("Message", data.toString())
+      })
+
   ws.on("close", (data) => {
     console.log("close", data);
   });
