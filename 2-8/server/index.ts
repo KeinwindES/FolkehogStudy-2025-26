738b2a02ea26472abaa050f7d978b2689f9b3070
Keinwind <KeinwindEichenSchwert@proton.me>
2025-11-26 12:42:05 +0100

    tretre

--- 2-8/server/index.ts
+++ 2-8/server/index.ts
@@ -1,19 +1,34 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({
   port: 8080,
 });
 
+const connections = new Map<number, WebSocket>();
+let connectionIdCounter = 0;
+
 wss.on("connection", ws => {
   console.log("connection");
+
+  const id = connectionIdCounter;
+  connectionIdCounter++;
+  connections.set(id, ws);
 
   ws.on("message", data => {
     const message = data.toString();
     switch (message) {
-      case "Hello from client!": break;
+        case "hei server!!":
+            console.log("hi")
+            break;
+        case "ekploder":
+            ws.send("boom")
+            connections.forEach((connection, connectionID) => {
+              connection.send("")
+            })
+            console.log("boom did run")
+            break;
     }
-    console.log("message", data.toString());
-  });
+    console.log("Message", data.toString())
 
   ws.on("close", data => {
     console.log("close", data);
