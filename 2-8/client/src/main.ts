728c802170735d5015e9aebac25c4ed95da93913
Keinwind <KeinwindEichenSchwert@proton.me>
2025-11-26 10:49:41 +0100

    sdfg

--- 2-8/client/src/main.ts
+++ 2-8/client/src/main.ts
@@ -1,3 +1,13 @@
 import 'style.css'
 
-const
+const websocket = new WebSocket("ws://127.0.0.1:8080");
+
+websocket.addEventListener("open", data => {
+  console.log("WebSocket opened", data);
+})
+websocket.addEventListener("message", data => {
+  console.log("Received message:", data);
+})
+websocket.addEventListener("close", data => {
+  console.log("WebSocket closed", data);
+})
