f4791f7039b77327af46eeee580cfc178f1ca03d
Keinwind <KeinwindEichenSchwert@proton.me>
2026-01-14 10:51:18 +0100

    Refactor frontend structure: move HTML, CSS, and JS files to a dedicated frontend directory; remove obsolete files

--- 2-1-2/frontend/index.js
+++ 2-1-2/frontend/index.js
@@ -3,6 +3,7 @@ let password = "";
 // Contains the username (from a html input field)
 let username = "";
 
+
 // Shpuld run when submitt button is clicked
 function Submit() {
     // Get the username and password from the html input fields
@@ -11,7 +12,7 @@ function Submit() {
     // Send the username and password to the server
     window.location.href = `YouLoggedIn.html`;
     // Check if the username and password are correct
-    if (username === "admin" && password === "password123") {
+    if (username === "admin" && password === "admin") {
             window.location.href = `Admin.html`;
     }
 }
