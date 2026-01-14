919813a5237b762e1e551c10aa620d984300ee0e
Keinwind <KeinwindEichenSchwert@proton.me>
2026-01-14 10:40:11 +0100

    Add login functionality and user feedback; create admin and logged-in pages

--- 2-1-2/index.js
+++ 2-1-2/index.js
@@ -5,5 +5,13 @@ let username = "";
 
 // Shpuld run when submitt button is clicked
 function Submit() {
-    
+    // Get the username and password from the html input fields
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+    // Send the username and password to the server
+    window.location.href = `YouLoggedIn.html`;
+    // Check if the username and password are correct
+    if (username === "admin" && password === "password123") {
+            window.location.href = `Admin.html`;
+    }
 }
