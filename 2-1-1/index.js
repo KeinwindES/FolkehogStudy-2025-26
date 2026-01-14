228fbc12af43291eb4c1100b77201e31e434c063
Keinwind <KeinwindEichenSchwert@proton.me>
2026-01-14 09:58:28 +0100

    implement MySQL connection and query functionality; add HTML structure for login page

--- 2-1-1/index.js
+++ 2-1-1/index.js
@@ -1,5 +1,24 @@
 const mysql = require("mysql2/promise");
 console .log("trying to connect to mysql");
-mysql.createConnection().then(e => {
-    console .log("connected to mysql");
-})
+
+process.loadEnvFile("./dev.env");
+
+const config = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DATABASEPASSWORD,
+    database: "world"
+
+};
+const pool = mysql.createPool(config);
+
+
+async function getAllCities() {
+    return await pool.query("Select * from city");
+}
+async function runApp() {
+    const cities = await getAllCities();
+    console.log(cities);
+}
+runApp();
---
+++ 2-1-2/index.js
@@ -0,0 +1,9 @@
+// Contains the password (from a html input field)
+let password = "";
+// Contains the username (from a html input field)
+let username = "";
+
+// Shpuld run when submitt button is clicked
+function Submit() {
+
+}
