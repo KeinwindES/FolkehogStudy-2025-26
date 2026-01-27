4c4fc63494ddbe380c5934cf45517af0f5bad8a3
Keinwind <KeinwindEichenSchwert@proton.me>
2026-01-27 12:51:41 +0100

    Add GLTFLoader import for enhanced model loading capabilities

--- 2-1-3/scene.js
+++ 2-1-3/scene.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
