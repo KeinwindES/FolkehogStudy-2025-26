2321aead4d082b99ed514578eefcea488d6205cd
Keinwind <KeinwindEichenSchwert@proton.me>
2026-01-28 10:10:42 +0100

    Add initial CSS styles for a pastel-themed layout
    - Introduced a new stylesheet (style.css) with styles for body, headings, forms, inputs, and buttons.
    - Set a pastel green background color and defined typography using 'Fira Code' font.
    - Implemented responsive design for form elements and button interactions.

---
+++ 2-1-4/main.js
@@ -0,0 +1,24 @@
+const canvas = document.getElementById('canvas');
+
+canvas.height = 800;
+canvas.width = 800;
+
+const ctx = canvas.getContext('2d');
+const background = '#000000';
+const foreground = '#2e31e9';
+
+ctx.fillStyle = background;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+function Point({x, y}, size) {
+    ctx.fillStyle = foreground;
+    ctx.fillRect(x, y, size, size);
+}
+
+function screen({x,y}){
+    x = (x + 1)*0.5 * canvas.width;
+    y = (y + 1)*0.5 * canvas.height;
+    return {x,y};
+}
+
+Point(screen({x: 0, y: 0}), 100);
